83d85610c3a6fcc3d7049b2326f4ad0bc3a87d6c
Vinicius <vinicius_baggio@hotmail.com>
2023-07-04 22:09:56 -0300

    Chapter 3 Section 3.1

---
+++ ch03/integers/integers.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Printf("%08b\n", x)    // "00100010", the set {1, 5}
+	fmt.Printf("%08b\n", y)    // "00000110", the set {1, 2}
+	fmt.Printf("%08b\n", x&y)  // "00000010", the intersection {1}
+	fmt.Printf("%08b\n", x|y)  // "00100110", the union {1, 2, 5}
+	fmt.Printf("%08b\n", x^y)  // "00100100", the symmetric difference {2, 5}
+	fmt.Printf("%08b\n", x&^y) // "00100000", the difference {5}
+	for i := uint(0); i < 8; i++ {
+		if x&(1<<i) != 0 { // membership test
+			fmt.Println(i) // "1", "5"
+		}
+	}
+	fmt.Printf("%08b\n", x<<1) // "01000100", the set {2, 6}
+	fmt.Printf("%08b\n", x>>1) // "00010001", the set {0, 4}
+
+	var b int16 = -128
+	fmt.Printf("b: %v %08b\n", b, b)
+	fmt.Printf("b: %v %08b\n", b>>1, b>>1)
+	fmt.Printf("b: %v %08b\n", b, b)
+
+	ascii := 'a'
+	unicode := 'D'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 D 'D'"
+	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
+}
