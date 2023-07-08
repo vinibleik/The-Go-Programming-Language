7b8d8f44c4b1b9ef215159d8c504f162587b06f7
vinibleik <vinicius_baggio@hotmail.com>
2023-07-08 11:33:09 -0300

    Chapter 3 Section 3.5

--- ch03/strings/comma.go
+++ ch03/strings/comma.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("%s\n", comma("12345"))
-	fmt.Printf("%s\n", comma("125"))
-	fmt.Printf("%s\n", comma("1234522420"))
+	// fmt.Printf("%s\n", comma("12345"))
+	// fmt.Printf("%s\n", comma("125"))
+	// fmt.Printf("%s\n", comma("1234522420"))
+
+	fmt.Printf("%s\n", comma_buffer("12345"))
+	fmt.Printf("%s\n", comma_buffer("125"))
+	fmt.Printf("%s\n", comma_buffer("1234522420"))
 }
 
 // comma insert commas in a non-negative decimal integer string
@@ -18,4 +23,14 @@ func comma(s string) string {
 	}
 
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func comma_buffer(s string) string {
+	var buf bytes.Buffer
+
+	for i := len(s) - 3; i >= 0; {
+		buf.WriteString(s[i:])
+	}
+
+	return buf.String()
 }
