3086e95e42d640e1cc5cd294e70aaf2025f03615
Vinicius <vinicius_baggio@hotmail.com>
2023-07-24 14:53:55 -0300

    Exercises 1.4-1.6

--- ch01/exercises/ex1.4/main.go
+++ ch01/exercises/ex1.4/main.go
@@ -9,37 +9,43 @@ import (
 	"os"
 )
 
+type CountLines struct {
+	count   int
+	foundIn []string
+}
+
 func main() {
-	counts := make(map[string]int)
-	foundIn := make(map[string][]string)
+
+	counts := make(map[string]*CountLines)
+
 	files := os.Args[1:]
 	if len(files) == 0 {
-		if err := countLines(os.Stdin, counts, foundIn); err != nil {
+		if err := countLines(os.Stdin, counts); err != nil {
 			log.Fatal("ex1.4: ", err)
 		}
 	} else {
 		for _, arg := range os.Args[1:] {
-			if err := wrapOpenFile(arg, counts, foundIn); err != nil {
+			if err := wrapOpenFile(arg, counts); err != nil {
 				fmt.Fprintf(os.Stderr, "ex1.4: %v\n", err)
 				continue
 			}
 		}
 	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("Count: %4d\tFound In: %v\tLine: %s\n", n, foundIn[line], line)
+	for line, data := range counts {
+		if data.count > 1 {
+			fmt.Printf("Count: %4d\tFound In: %v\tLine: %s\n", data.count, data.foundIn, line)
 		}
 	}
 }
 
-func wrapOpenFile(file string, counts map[string]int, foundIn map[string][]string) error {
+func wrapOpenFile(file string, counts map[string]*CountLines) error {
 	f, err := os.Open(file)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	if err := countLines(f, counts, foundIn); err != nil {
+	if err := countLines(f, counts); err != nil {
 		return err
 	}
 	return nil
@@ -54,13 +60,17 @@ func in(curFile string, files []string) bool {
 	return false
 }
 
-func countLines(f *os.File, counts map[string]int, foundIn map[string][]string) error {
+func countLines(f *os.File, counts map[string]*CountLines) error {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		line := input.Text()
-		counts[line]++
-		if !in(f.Name(), foundIn[line]) {
-			foundIn[line] = append(foundIn[line], f.Name())
+		_, ok := counts[line]
+		if !ok {
+			counts[line] = new(CountLines)
+		}
+		counts[line].count++
+		if !in(f.Name(), counts[line].foundIn) {
+			counts[line].foundIn = append(counts[line].foundIn, f.Name())
 		}
 	}
 
